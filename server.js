7adaca498d6e6b8066adb30875d983d4d3a09bb3
Michael Daniels <mikeytdan@gmail.com>
2018-08-27 23:24:53 -0600

    Updated view paths

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true });
 var app = express();
 
 // view engine setup
-app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.engine('.hbs', expressHbs({
+  defaultLayout: 'layout',
+  extname: '.hbs',
+  layoutsDir: './views/layouts',
+  partialsDir: './views/partials'
+}));
 app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
@@ -29,12 +34,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
